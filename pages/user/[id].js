a9c6b52220deb33de03b1a6350f8b0d9e5661347
KAMONPAT SUNTHONPONG <kamonpat_s@cmu.ac.th>
2021-10-14 23:12:03 +0700

    user main page

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 const baseURL = 'http://127.0.0.1:8000/api'
 
 export default function Address(){
 
     return (
         <>
-            <tr><Link href= "">info</Link></tr>
             <tr><Link href="">customers</Link></tr>
-            <tr><Link href=""></Link></tr>
+            <tr><Link href="">stock</Link></tr> {/*for sales employee */}
+            <tr><Link href="">orders</Link></tr> {/*for sales employee */}
+            <tr><Link href="">paymentation</Link></tr> {/*for sales employee*/}
+            <tr><Link href="">employee management</Link></tr> {/*for VP sales only*/}
         </>
     )
 }
