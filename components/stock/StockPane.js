9d8113bdc78d109d711fda93ff92f9934df34c43
mook30888 <mookkak@gmail.com>
2021-10-26 19:10:07 +0700

    stock pane table add

--- components/stock/StockPane.js
+++ components/stock/StockPane.js
@@ -2,14 +2,54 @@ import { useState, useEffect, createContext, useContext} from 'react'
 import axios from 'axios'
 import { Table, Button, Offcanvas } from 'react-bootstrap'
 import { StockinContext } from './StockInList'
+const baseURL = 'http://127.0.0.1:8000/api'
 
 export default function StockPane() {
-    const [products, setProduct] = useState([])
-    //const {catalog} = useContext(StockinContext)
+    const [stockins, setstockins] = useState([])
+    const {catalog} = useContext(StockinContext)
+    const fetch = () =>{
+        
+            axios.get(`${baseURL}/stock`)
+            .then(response => {
+                setstockins(response.data)
+            })
+    }
+
+    useEffect(fetch,[])
+    useEffect(()=>{
+        localStorage.setItem('stockins',JSON.stringify(stockins))
+    },[stockins])
+
 
     return (
         <div>
-            <p>????????????????? </p>
+            <Table striped>
+            <thead>
+            <tr>
+                <td><h5>Product Code</h5></td>
+                <td><h5>Quantity</h5></td>
+                <td><h5>Create at</h5></td>
+                <td><h5>Update at</h5></td>
+
+            </tr>
+            </thead>
+            <tbody>
+            {
+                    stockins.map(c=>{
+                        return(
+                            <tr key={c.productCode}>
+                                <td>{c.productCode}</td>
+                                <td>{c.qty}</td>
+                                <td>{c.created_at}</td>
+                                <td>{c.updated_at}</td>
+                                <td><Button variant="success">Edit</Button> {' '}
+                                <Button variant="danger" onClick={()=>{del(c.produtCode)}}>Delete</Button></td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </Table>
         </div>
     )
 }
