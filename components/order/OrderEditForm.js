a50c1d6110ea67aad818039b33617905b4aeb81b
KAMONPAT SUNTHONPONG <kamonpat_s@cmu.ac.th>
2021-10-26 16:15:00 +0700

    edit form renovated

--- components/order/OrderEditForm.js
+++ components/order/OrderEditForm.js
@@ -6,6 +6,9 @@ const orderURL = "http://127.0.0.1:8000/api/order";
 
 export default function OrderEditForm() {
     const {orders, selected, setShow, setSelect, setOrders} = useContext(OrderContext)
+    const actions = ["In Process", "On Hold",
+                    "Resolved", "Shipped",
+                    "Canceled","Disputed"]
     
     const submitHandler = (e) => {
         e.preventDefault();
@@ -31,28 +34,36 @@ export default function OrderEditForm() {
     return (
     <div>
         <Form>
-            <Form.Group className="mb-3" controlId="formGridShippedDate">
-                <Form.Label>Shipped Date</Form.Label>
-                <Form.Control value={selected.shippedDate} placeholder="YYYY-MM-DD" onChange={e=>{
-                    setSelect({...selected,shippedDate:e.target.value})
-                }}/>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formGridStatus">
-                <Form.Label>Status</Form.Label>
-                <Form.Control value={selected.status} placeholder="in process, resolved, canceled etc." onChange={e=>{
-                    setSelect({...selected,status:e.target.value})
-                }}/>
-            </Form.Group>
 
             <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridComments">
-                <Form.Label>Comments</Form.Label>
-                <Form.Control value={selected.comments} onChange={e=>{
-                    setSelect({...selected,comments:e.target.value})
-                }}/>
+                <Form.Group as={Col} controlId="formGridShippedDate">
+                    <Form.Label>Shipped Date</Form.Label>
+                    <Form.Control type='date' value={selected.shippedDate} onChange={e=>{
+                        setSelect({...selected,shippedDate:e.target.value})
+                    }}/>
+                </Form.Group>
+
+                <Form.Group  as={Col} controlId="formGridStatus">
+                    <Form.Label>Status</Form.Label>
+                    <Form.Select onChange={e=>{
+                        setSelect({...selected,status:e.target.value})
+                    }}>
+                    {
+                        actions.map(action=>{
+                            return (
+                                <option selected={selected.status == action} value={action}>{action}</option>
+                            )
+                        })
+                    }
+                    </Form.Select>
                 </Form.Group>
             </Row>
+            <Form.Group controlId="formGridComments">
+            <Form.Label>Comments</Form.Label>
+            <Form.Control as='textarea' value={selected.comments} onChange={e=>{
+                setSelect({...selected,comments:e.target.value})
+            }}/>
+            </Form.Group>
 
             <Button variant="primary" onClick={submitHandler}>
                 Submit
